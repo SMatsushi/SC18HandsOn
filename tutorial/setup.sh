bdc96f8d3eaf2611da5fa8fe8c66800e5e551aa3
Satoshi Matsushita <satoshi.matsushita@necect.com>
2019-05-27 06:05:51 -0700

    For ISC19. Added sdk 2.1 etc.

--- tutorial/setup.sh
+++ tutorial/setup.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-if [ -d /opt/nec/ve/mpi/1.2.0 ]; then
-	cmd="source /opt/nec/ve/mpi/1.2.0/bin/necmpivars.sh"
+if [ -d /opt/nec/ve/mpi/2.1.0 ]; then
+	cmd="source /opt/nec/ve/mpi/2.1.0/bin/necmpivars.sh"
 elif [ -d /opt/nec/ve/mpi/1.3.0 ]; then
 	cmd="source /opt/nec/ve/mpi/1.3.0/bin/necmpivars.sh"
-else
+elif [ -d /opt/nec/ve/mpi/1.2.0 ]; then
+	cmd="source /opt/nec/ve/mpi/1.2.0/bin/necmpivars.sh"
+elif [ -d /opt/nec/ve/mpi/1.1.0 ]; then
 	cmd="source /opt/nec/ve/mpi/1.1.0/bin/necmpivars.sh"
+else
+	echo 'Error: Cannot find necmpivars.sh in /opt/nec/ve/mpi/*.*.*'
+	exit 1
 fi
 
 echo $cmd
